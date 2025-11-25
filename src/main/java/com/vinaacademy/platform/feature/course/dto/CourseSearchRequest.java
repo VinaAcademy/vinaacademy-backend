a18033eb4f66c264333cc290b97b5ccbc9a868ff
Nguyen Huu Loc <locn562836@gmail.com>
2025-11-25 19:28:42 +0700

    feat(course): add support for multiple category slugs in course search and specifications

--- src/main/java/com/vinaacademy/platform/feature/course/dto/CourseSearchRequest.java
+++ src/main/java/com/vinaacademy/platform/feature/course/dto/CourseSearchRequest.java
@@ -8,6 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -17,6 +18,7 @@ import java.util.UUID;
 public class CourseSearchRequest {
     private String keyword;
     private String categorySlug;
+    private List<String> categorieSlugs;
     private UUID instructorId;
     private CourseLevel level;
     private String language;
